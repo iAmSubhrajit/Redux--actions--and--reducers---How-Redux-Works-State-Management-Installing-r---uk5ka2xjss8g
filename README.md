b4a8b71357af0e26b6fd95fc5cbcaef21f8f85c9
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2023-01-28 12:28:07 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# boilerplate-react-functional-public
+# Redux- actions- and- reducers
+Complete the files
+1. app.js
+2. actions.js
+3. reducers.js
+4. index.js
+
+You are given an initial state(loggedIn: false, username: '').
+Complete the login functionality
+1. input tag to take the username of the user with id 'username'
+
+2. login button with id="login-button" on click dispatch respective action.
+
+3. When use is loggedIn display the output 'Hello username' in a span element with id 'user'
+
+4. If the user is loggedIn show logout button with id 'logout-button'.
+
+5. Use appropriate actions and reducers to store username and loggedIn the state.
+For login, use
+
+[Demo Video](https://d3dyfaf3iutrxo.cloudfront.net/general/upload/473835d90e8c4aacbe2cc3b35ff07b21.webm)
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react'
+import { useRef } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { onSignIn, onSignOut } from '../actions';
 import '../styles/App.css';
 
+
 const LoginButton = () => {
+  const dispatch = useDispatch();
+  const inputRef = useRef(null)
+
+  return (
+    <>
+      <input type='text' ref={inputRef} id='username' />
+      <button id="login-button" type='button' onClick={() => dispatch(onSignIn(inputRef.current.value))}>Login</button>
+    </>
+  )
 }
 
 const LogoutButton = () => {
+  const dispatch = useDispatch();
+  const username = useSelector((state) => state.auth.username)
+  return (
+    <>
+      <span id='user'>Hello {username}</span>
+      <button id='logout-button' type='button' onClick={() => dispatch(onSignOut())}>Logout</button>
+    </>
+  )
 }
 
 const App = () => {
@@ -13,10 +33,7 @@ const App = () => {
 
   return (
     <div id="main">
-      {loggedIn
-        ? <LogoutButton />
-        : <LoginButton />
-      }
+      {loggedIn ? <LogoutButton /> : <LoginButton />}
     </div>
   )
 }
